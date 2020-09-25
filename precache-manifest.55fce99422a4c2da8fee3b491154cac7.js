b5bae07eb160da0c24e993188e6626c2d6c3c6be
keval8solanki <keval8solanki@gmail.com>
2020-09-25 16:27:30 +0530

    Updates

---
+++ precache-manifest.55fce99422a4c2da8fee3b491154cac7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18cc63d557eb32c4ccbd9486984570c9",
+    "url": "/sos-dashboard/index.html"
+  },
+  {
+    "revision": "27ad9bcacdaa954d1738",
+    "url": "/sos-dashboard/static/css/main.71ed0570.chunk.css"
+  },
+  {
+    "revision": "bafba4eddb8dd7ac0162",
+    "url": "/sos-dashboard/static/js/2.b275b71f.chunk.js"
+  },
+  {
+    "revision": "e48cce1e44c25543cb9e68ec58e85994",
+    "url": "/sos-dashboard/static/js/2.b275b71f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ad9bcacdaa954d1738",
+    "url": "/sos-dashboard/static/js/main.29890f62.chunk.js"
+  },
+  {
+    "revision": "e1aa88ec7ba1a3fceab2",
+    "url": "/sos-dashboard/static/js/runtime-main.e6712657.js"
+  },
+  {
+    "revision": "15cf12cecebee2fc60ff6fb067224a02",
+    "url": "/sos-dashboard/static/media/add.15cf12ce.svg"
+  },
+  {
+    "revision": "910669c3e33147e873e966d22a5cb313",
+    "url": "/sos-dashboard/static/media/auth.910669c3.svg"
+  },
+  {
+    "revision": "5138f3241259c0433b4659fa54eddc55",
+    "url": "/sos-dashboard/static/media/candidate.5138f324.svg"
+  },
+  {
+    "revision": "5c37dd19f5d560127bf0d08b3d581b65",
+    "url": "/sos-dashboard/static/media/company.5c37dd19.svg"
+  },
+  {
+    "revision": "3949fa5bf4d51f5e8996a43dcf71b225",
+    "url": "/sos-dashboard/static/media/dashboard.3949fa5b.svg"
+  },
+  {
+    "revision": "df887e14d808d5219347c3353554a250",
+    "url": "/sos-dashboard/static/media/job.df887e14.svg"
+  },
+  {
+    "revision": "d9a6520ebd171dd0deb6f0e4c93cb23a",
+    "url": "/sos-dashboard/static/media/loader.d9a6520e.svg"
+  },
+  {
+    "revision": "c4676091e214a123133e1da79e1bd4cb",
+    "url": "/sos-dashboard/static/media/profile.c4676091.svg"
+  },
+  {
+    "revision": "9f9a73291c417d8799905ca5d77dcd62",
+    "url": "/sos-dashboard/static/media/role.9f9a7329.svg"
+  },
+  {
+    "revision": "19142cc51b8c074217df3d7499267bf0",
+    "url": "/sos-dashboard/static/media/search.19142cc5.svg"
+  },
+  {
+    "revision": "a0b238c9eb9dba2b5a1003c20d66860d",
+    "url": "/sos-dashboard/static/media/user.a0b238c9.svg"
+  }
+]);
